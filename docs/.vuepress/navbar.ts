c430ada37ea4bb4af6375db102c4039f10612c93
w509 <2607042493@qq.com>
2025-06-17 00:48:04 +0800

    icon

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,16 +1,17 @@
 import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
-  { text: '首页', link: '/' },
-  { text: '文章', link: '/blog/' },
+  { text: '首页', link: '/', icon: 'mdi:home' },
+  { text: '文章', link: '/blog/', icon: 'material-symbols:article' },
   //{ text: '标签', link: '/tags/' },
-  { text: '归档', link: '/archives/' },
-  { text: '分类', link: '/categories/' },
+  { text: '归档', link: '/archives/', icon: 'material-symbols:archive' },
+  { text: '分类', link: '/categories/', icon: 'quill:sort' },
   {
     text: '笔记',
+    icon: 'material-symbols:edit-note',
     items: [
-      { text: 'STM32', link: '/notes/STM32/README.md', icon: 'ph:info-light' },
-      { text: 'MaixPy', link: '/notes/MaixPy/README.md' },
+      { text: 'STM32', link: '/notes/STM32/README.md', icon: 'codicon:chip' },
+      { text: 'MaixPy', link: '/notes/MaixPy/README.md', icon: 'mdi:eye' },
     ]
   },
 ])
